157c30ea55e50ffc831eee9e41c78091641c6bc0
7box7 <60572459+7box7@users.noreply.github.com>
2021-06-11 01:36:07 +0300

    Update settings.py

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -77,7 +77,12 @@ WSGI_APPLICATION = 'vercel_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
